5bff6f3d47719fb7309cf930d9c66021ac08f922
Sorin Birsan <sorin.birsan.bp@nttdata.ro>
2021-03-04 16:25:46 +0100

    rename generated class Class1 to Calculaor and add some logic

--- ClassLibraryNUnit/Calculator.cs
+++ ClassLibraryNUnit/Calculator.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryNUnit
 {
-    public class Class1
-    {
-    }
+	public class Calculator
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+		public int Subtract(int a, int b)
+		{
+			return a - b;
+		}
+	}
 }
