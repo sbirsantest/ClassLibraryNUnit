7470f67412ced47340b2490c7357bbdcbba8647f
sbarsan <sorinbarsan@gmail.com>
2021-03-05 20:04:19 +0200

    read data from excel file and create some tests based on these data

---
+++ ClassLibraryNUintWithExcel.Tests/CalculatorTestData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryNUintWithExcel.Tests
+{
+	public class CalculatorTestData
+	{
+		private readonly int _a;
+		private readonly int _b;
+		private readonly int _result;
+
+		public int A { get => _a;}
+		public int B { get => _b; }
+		public int Result { get => _result; }
+
+		public CalculatorTestData()
+		{
+			_a = 0;
+			_b = 0;
+			_result = 0;
+		}
+
+		public CalculatorTestData(int a, int b, int result)
+		{
+			_a = a;
+			_b = b;
+			_result = result;
+		}
+	}
+}
--- ClassLibraryNUintWithExcel.Tests/CalculatorTestsWithExcel.cs
+++ ClassLibraryNUintWithExcel.Tests/CalculatorTestsWithExcel.cs
@@ -1,53 +1,262 @@
+using ClassLibraryNUnit;
 using ExcelDataReader;
 using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Linq;
+
+[SetUpFixture]
+public static class ExcelHelper
+{
+	private static DataSet _dataSet;
+
+	private static List<ClassLibraryNUintWithExcel.Tests.CalculatorTestData> _calculatorAddTestData;
+
+	private static List<ClassLibraryNUintWithExcel.Tests.CalculatorTestData> _calculatorSubtarctTestData;
+
+	public static List<ClassLibraryNUintWithExcel.Tests.CalculatorTestData> CalculatorAddTestData
+	{
+		get { return _calculatorAddTestData; }
+		set { _calculatorAddTestData = value; }
+	}
+
+	public static List<ClassLibraryNUintWithExcel.Tests.CalculatorTestData> CalculatorSubtarctTestData
+	{
+		get { return _calculatorSubtarctTestData; }
+		set { _calculatorSubtarctTestData = value; }
+	}
+	////[OneTimeSetUp]
+	//public static void AssemblySetup()
+	//{
+	//	try
+	//	{
+	//		LoadTestDataFromExcel();
+	//		SetAddTestData();
+	//	}
+	//	catch (Exception)
+	//	{
+	//		throw;
+	//	}
+	//}
+
+	public static void LoadTestDataFromExcel()
+	{
+		try
+		{
+			if (_dataSet == null)
+			{
+				System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+				using (var stream = File.Open(@"TestFiles\CalculatorTestData.xlsx", FileMode.Open, FileAccess.Read))
+				{
+					using (var reader = ExcelReaderFactory.CreateReader(stream))
+					{
+						_dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+						{
+							ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+							{
+								UseHeaderRow = true
+							}
+						});
+					}
+				}
+			}
+		}
+		catch (Exception)
+		{
+			throw;
+		}
+	}
+
+	public static void SetAddTestData()
+	{
+		try
+		{
+			var addTestDataTable = _dataSet?.Tables["AddTestData"];
+
+			if (addTestDataTable != null)
+			{
+				_calculatorAddTestData = (from testDataRow in addTestDataTable.AsEnumerable()
+												  select
+												  new ClassLibraryNUintWithExcel.Tests.CalculatorTestData
+													 (
+														 Convert.ToInt32(testDataRow["InputData1"]),
+														 Convert.ToInt32(testDataRow["InputData2"]),
+														 Convert.ToInt32(testDataRow["ExpectedResult"])
+													 )
+								 ).ToList();
+			}
+		}
+		catch (Exception)
+		{
+			throw;
+		}
+	}
+
+	public static void SetSubtractTestData()
+	{
+		try
+		{
+			var addTestDataTable = _dataSet?.Tables["SubtractTestData"];
+
+			if (addTestDataTable != null)
+			{
+				_calculatorSubtarctTestData = (from testDataRow in addTestDataTable.AsEnumerable()
+												  select
+												  new ClassLibraryNUintWithExcel.Tests.CalculatorTestData
+													 (
+														 Convert.ToInt32(testDataRow["InputData1"]),
+														 Convert.ToInt32(testDataRow["InputData2"]),
+														 Convert.ToInt32(testDataRow["ExpectedResult"])
+													 )
+								 ).ToList();
+			}
+		}
+		catch (Exception)
+		{
+			throw;
+		}
+	}
+}
+
 
 namespace ClassLibraryNUintWithExcel.Tests
 {
-	public static class ExcelHelper
+	public class CalculatorFactoryAddTestCases : List<TestCaseData>
 	{
-		public static DataSet _dataSet;
-
-		public static DataSet LoadTestDataFromExcel()
+		public CalculatorFactoryAddTestCases()
 		{
-			using (var stream = File.Open(@"TestFiles\CalculatorTestData.xlsx", FileMode.Open, FileAccess.Read))
+			try
 			{
-				using (var reader = ExcelReaderFactory.CreateReader(stream))
+				ExcelHelper.LoadTestDataFromExcel();
+				ExcelHelper.SetAddTestData();
+				foreach (var item in ExcelHelper.CalculatorAddTestData)
 				{
-					_dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
-					{
-						ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
-						{
-							UseHeaderRow = true
-						}
-					});
+					Add(new TestCaseData(item));
 				}
 			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
 
-			return _dataSet;
+	public class CalculatorFactorySubtarctTestCases : List<TestCaseData>
+	{
+		public CalculatorFactorySubtarctTestCases()
+		{
+			try
+			{
+				ExcelHelper.LoadTestDataFromExcel();
+				ExcelHelper.SetSubtractTestData();
+				foreach (var item in ExcelHelper.CalculatorSubtarctTestData)
+				{
+					Add(new TestCaseData(item));
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+
+	public class CalculatorFactoryTestCases
+	{
+		public CalculatorFactoryTestCases()
+		{
+			ExcelHelper.LoadTestDataFromExcel();
+		}
+
+		public static IEnumerable AddTestCases
+		{
+			get
+			{
+				ExcelHelper.LoadTestDataFromExcel();
+				ExcelHelper.SetAddTestData();
+				foreach (var item in ExcelHelper.CalculatorAddTestData)
+				{
+					yield return item;
+				}
+			}
+		}
+
+		public static IEnumerable SubtractTestCases
+		{
+			get
+			{
+				ExcelHelper.LoadTestDataFromExcel();
+				ExcelHelper.SetSubtractTestData();
+				foreach (var item in ExcelHelper.CalculatorSubtarctTestData)
+				{
+					yield return new TestCaseData(item);
+				}
+			}
 		}
 	}
 
 	public class CalculatorTestsWithExcel
 	{
+		private Calculator _calculator;
+
 		[OneTimeSetUp]
 		public void TestFixtureSetUp()
 		{
 			// Setup once per fixture
-			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-			var testData = ExcelHelper.LoadTestDataFromExcel();
+			//ExcelHelper.LoadTestDataFromExcel();
 		}
 
 		[SetUp]
 		public void Setup()
 		{
+			_calculator = new Calculator();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			//_calculator.Dispose();
+			_calculator = null;
 		}
 
 		[Test]
-		public void Test1()
+		[Ignore("")]
+		public void Add_ShouldReturn5()
 		{
-			Assert.Pass();
+			var result = _calculator.Add(2, 3);
+			Assert.That(result, Is.EqualTo(5));
+		}
+
+		[TestCaseSource(typeof(CalculatorFactoryTestCases), "AddTestCases")]
+		public void Calculator_Add1_ShouldReturnExpectedResult(CalculatorTestData ctd)
+		{
+			var result = _calculator.Add(ctd.A, ctd.B);
+			Assert.That(result, Is.EqualTo(ctd.Result));
+		}
+
+		[TestCaseSource(typeof(CalculatorFactoryAddTestCases))] 
+		public void Calculator_Add2_ShouldReturnExpectedResult(CalculatorTestData ctd)
+		{
+			var result = _calculator.Add(ctd.A, ctd.B);
+			Assert.That(result, Is.EqualTo(ctd.Result));
+		}
+
+		[TestCaseSource(typeof(CalculatorFactoryTestCases), "SubtractTestCases")]
+		public void Calculator_Subtract_ShouldReturnExpectedResult(CalculatorTestData ctd)
+		{
+			var result = _calculator.Subtract(ctd.A, ctd.B);
+			Assert.That(result, Is.EqualTo(ctd.Result));
+		}
+
+		[TestCaseSource(typeof(CalculatorFactorySubtarctTestCases))] 
+		public void Calculator_Subtract2_ShouldReturnExpectedResult(CalculatorTestData ctd)
+		{
+			var result = _calculator.Subtract(ctd.A, ctd.B);
+			Assert.That(result, Is.EqualTo(ctd.Result));
 		}
 	}
 }
