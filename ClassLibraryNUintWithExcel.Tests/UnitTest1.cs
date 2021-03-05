2626b094ad567e022494799527579442d91c068b
sbarsan <sorinbarsan@gmail.com>
2021-03-05 11:50:16 +0200

    add new nunit test project to be used for tests using data from excel files

---
+++ ClassLibraryNUintWithExcel.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ClassLibraryNUintWithExcel.Tests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
